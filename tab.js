4875821f4b9025ff49aaf135d144b6916d969dff
chaebeen <tnwl05200@gmail.com>
2024-04-26 13:38:17 +0900

    input

--- tab.js
+++ tab.js
@@ -1,13 +1,29 @@
-for(let i = 0; i < $('.tab-content').length; i++) {
-    $('.tab-button').eq(i).on('click' , function(){
-        $('.tab-button').removeClass('orange');
-        $('.tab-button').eq(i).addClass('orange');
-        $('.tab-content').removeClass('show');
-        $('.tab-content').eq(i).addClass('show');
-    })    
+// for(let i = 0; i < $('.tab-content').length; i++) {
+//     $('.tab-button').eq(i).on('click' , function(){
+    
+//         탭열기(i);
+//     })    
+// }
+
+function 탭열기(구멍){
+    $('.tab-button').removeClass('orange');
+    $('.tab-button').eq(구멍).addClass('orange');
+    $('.tab-content').removeClass('show');
+    $('.tab-content').eq(구멍).addClass('show');
 }
 
+$('.list').click(function(e){
 
+    if(e.target == document.querySelectorAll('.tab-button')[0]){
+        탭열기(0);
+    }
+    if(e.target == document.querySelectorAll('.tab-button')[1]){
+        탭열기(1);
+    }
+    if(e.target == document.querySelectorAll('.tab-button')[2]){
+        탭열기(2);
+    }
+})
 
 
 
